3cb6ca4fee1b97cf3ecc89990d3495c354f24396
ajmarrocco <ajmarrocco@gmail.com>
2022-02-23 21:36:31 -0500

    update animal files

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
+
 
